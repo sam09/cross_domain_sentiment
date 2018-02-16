126389a001aab48c36b6337eff8acbed03839710
Sam Radhakrishnan <saradhak@amazon.com>
2018-02-17 03:10:44 +0530

    Add updates to the model

--- cnn.py
+++ cnn.py
@@ -1,4 +1,4 @@
-from keras.layers import Convolution2D, MaxPooling2D, Dense, Dropout, Flatten
+from keras.layers import Convolution2D, MaxPooling2D, Dense, Dropout, Flatten, AveragePooling2D
 from keras.utils import np_utils
 from keras.models import Sequential
 
@@ -10,14 +10,14 @@ class CNN:
 
 	def get_model(self):
 		cnn = Sequential()
-		cnn.add(Convolution2D(128, 3, 1 , activation="relu", border_mode="same", input_shape = (1,self.size,1) ))
-		cnn.add(Convolution2D(128, 3, 1, border_mode="same", activation="relu"))
-		cnn.add(Convolution2D(128, 5, 1, border_mode="same", activation="relu"))
-		cnn.add(MaxPooling2D(pool_size=(self.size - 2, 1), strides=(1,1), border_mode="same" ))
-		cnn.add(MaxPooling2D(pool_size=(self.size - 3, 1), strides=(1,1), border_mode="same" ))
-		cnn.add(MaxPooling2D(pool_size=(self.size - 4, 1), strides=(1,1), border_mode="same" ))
+		cnn.add(Convolution2D(50, kernel_size=(3,1), strides=(1,1) , activation="relu", padding="same", input_shape = (1,self.size,1) ))
+		cnn.add(Convolution2D(50, kernel_size=(4,1), strides=(1,1) , activation="relu", padding="same", input_shape = (1,self.size,1) ))
+		cnn.add(Convolution2D(50, kernel_size=(5,1), strides=(1,1) , activation="relu", padding="same", input_shape = (1,self.size,1) ))
+		cnn.add(MaxPooling2D(pool_size=(self.size - 2, 1), strides=(1,1), padding="same" ))
+		cnn.add(MaxPooling2D(pool_size=(self.size - 3, 1), strides=(1,1), padding="same" ))
+		cnn.add(MaxPooling2D(pool_size=(self.size - 4, 1), strides=(1,1), padding="same" ))
 		cnn.add(Flatten())
-		cnn.add(Dense(128, activation="relu"))
+		cnn.add(Dense(128, activation="softmax"))
 		cnn.add(Dropout(0.5))
 		cnn.add(Dense(2, activation="softmax"))
 		cnn.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
--- doc2vec.py
+++ doc2vec.py
@@ -8,15 +8,21 @@ from sklearn.linear_model import LogisticRegression as LR
 from sklearn.svm import SVC
 from keras.utils import np_utils
 from random import shuffle
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score
+
 from gensim.test.test_doc2vec import ConcatenatedDoc2Vec
-
-train_path = ["Data/train/DVDTrain.csv", "Data/train/KitchenTrain.csv", "Data/ElectronicsUnlabel.csv" "Data/train/DVDTrain.csv"]
-
-test_path = "Data/train/ElectronicsTrain.csv"
-print train_path, test_path
 SentimentDocument = namedtuple("SentimentDocument", "words tags sentiment split")
 
-alldocs = []
+train = [
+		 "./Data/train/BookTrain.csv",
+	     "./Data/train/DVDTrain.csv", 
+		 "./Data/train/ElectronicsTrain.csv",
+		 "./Data/train/KitchenTrain.csv"
+]
+test = ["./Data/test/BookTest.csv","./Data/test/DVDTest.csv","./Data/test/ElectronicsTest.csv","./Data/test/KitchenTest.csv"]
+unlabel = ["./Data/BookUnlabel.csv", "./Data/DVDUnlabel.csv", "./Data/ElectronicsUnlabel.csv", "./Data/KitchenUnlabel.csv"]
+
+test_path = unlabel[1]
 
 def sentiment_to_int(i):
 	if i == "pos":
@@ -36,6 +42,7 @@ def get_data(file_path, split="train"):
 	f = open(file_path)
 	f.readline()
 	k = 0
+	docs = []
 	for i in f.readlines():
 		s = i.split(",")
 		words = ""
@@ -46,8 +53,10 @@ def get_data(file_path, split="train"):
 		tags =[split + "_" + str(k)]
 		sentiment = sentiment_to_int(s[0])
 		words = to_unicode(words).split()
-		alldocs.append(SentimentDocument(words, tags, sentiment, split))
+		docs.append(SentimentDocument(words, tags, sentiment, split))
 		k+=1
+	f.close()
+	return docs
 
 def split(docs, model):
 	X = []
@@ -56,7 +65,15 @@ def split(docs, model):
 		Y.append(i.sentiment)
 		X.append(model.docvecs[i.tags[0]])
 	return X, Y
-	
+
+def split_test(docs, model):
+	X = []
+	Y = []
+	for i in docs:
+		Y.append(i.sentiment)
+		X.append(model.infer_vector(i.words))
+	return X, Y
+
 def get_accuracy(pred, testY):
 	c = 0
 	pred = [round(x) for x in  pred]
@@ -69,83 +86,147 @@ def get_accuracy(pred, testY):
 	print len(testY)
    	return c
 
-get_data(train_path[0])
-#get_data(train_path[2])
-get_data(train_path[1])
-
-get_data(test_path, split="test")
-
-train_docs = [doc for doc in alldocs if doc.split == 'train']
-test_docs = [doc for doc in alldocs if doc.split == 'test']
 
 simple_models = [
     # PV-DM w/concatenation - window=5 (both sides) approximates paper's 10-word total window size
-    Doc2Vec(dm=1, dm_concat=1, size=100, window=5, negative=5, hs=0, min_count=2),
-    # PV-DBOW 
-    Doc2Vec(dm=0, size=100, negative=5, hs=0, min_count=2),
-    # PV-DM w/average
-    Doc2Vec(dm=1, dm_mean=1, size=100, window=10, negative=5, hs=0, min_count=2)
+    Doc2Vec(dm=1, dm_concat=1, vector_size=100, window=5, negative=5, hs=0, min_count=2, epochs=55),
+		    # PV-DBOW 
+	Doc2Vec(dm=0, vector_size=100, negative=5, hs=0, min_count=2, epochs=100),
+		    # PV-DM w/average
+	Doc2Vec(dm=1, dm_mean=1, vector_size=100, window=10, negative=10, hs=0, min_count=2, epochs=55)
 ]
 
-for model in simple_models:
-	model.build_vocab(alldocs)
 
-models_by_name = []
+train_docs = []
+for i in range(len(train)):
+	train_docs.extend(get_data(train[i]))
 
-models_by_name.append(ConcatenatedDoc2Vec([simple_models[1], simple_models[2]]))
+test_docs = get_data(test_path, split="test")
+
+simple_models[1].build_vocab(train_docs)
+models_by_name =[]
+
+#models_by_name.append(simple_models[0])
+models_by_name.append(simple_models[1])
+#models_by_name.append(simple_models[2])
+#models_by_name.append(ConcatenatedDoc2Vec([simple_models[1], simple_models[2]]))
+
+doc_list = train_docs
+shuffle(doc_list)
+for i in range(len(models_by_name)):
+	if i<3:
+		models_by_name[i].train(doc_list, epochs=models_by_name[i].epochs, total_examples=models_by_name[i].corpus_count)
+	else:
+		models_by_name[i].train(doc_list)
 
 
-doc_list = alldocs[:]
+for train_i in range(len(train)):
+	for test_i in range(4):
+		train_path = train[train_i]
+		train_docs = get_data(train_path)
 
-passes = 10
+		simple_models = [
+		    # PV-DM w/concatenation - window=5 (both sides) approximates paper's 10-word total window size
+		    Doc2Vec(dm=1, dm_concat=1, vector_size=100, window=5, negative=5, hs=0, min_count=2, epochs=55),
+		    # PV-DBOW 
+		    Doc2Vec(dm=0, vector_size=100, negative=5, hs=0, min_count=2, epochs=100),
+		    # PV-DM w/average
+		    Doc2Vec(dm=1, dm_mean=1, vector_size=100, window=10, negative=10, hs=0, min_count=2, epochs=55)
+		]
 
-for epoch in range(passes):
-	shuffle(doc_list)
-	for i in models_by_name:
-		model.train(doc_list)
-	print epoch,  " Done" 
+		simple_models[1].build_vocab(train_docs)
+		models_by_name =[]
 
-#training with SVC
+		#models_by_name.append(simple_models[0])
+		models_by_name.append(simple_models[1])
+		#models_by_name.append(simple_models[2])
+		#models_by_name.append(ConcatenatedDoc2Vec([simple_models[1], simple_models[2]]))
 
-for model in models_by_name:
-	trainX, trainY  = split(train_docs, model)
-	testX, testY = split(test_docs, model)
-	linear_model = SVC(probability=True)
-	linear_model.fit(trainX, trainY)
-	pred = linear_model.predict_proba(testX)
-	c = 0
-	tot =0
-	for i in range(0, len(pred)):
-		if pred[i][0] >= 0.60 or pred[i][0] <=0.40:
-			tot += 1
-		if pred[i][0] > 0.5 and testY[i] == 0:
-			c+=1
-		elif testY[i]==1 and pred[i][0] <0.5:
-			c+=1
 
-	#test_accuracy = get_accuracy(pred, testY)
-	c*=1.0
-	tot *= 1.0
-	test_accuracy = c/tot
-	print test_accuracy
+		doc_list = train_docs
+		shuffle(doc_list)
+		for i in range(len(models_by_name)):
+			if i<3:
+				models_by_name[i].train(doc_list, epochs=models_by_name[i].epochs, total_examples=models_by_name[i].corpus_count)
+			else:
+				models_by_name[i].train(doc_list)
 
-#training with CNN
+		for test_j in range(2):
+			test_path = test[test_i]
+			title = "Test Data"
+			if test_j==1:
+				test_path = unlabel[test_i]
+				title = "Cross  Validation Set(Larger Data Set)"
+
+			print title
+			test_docs = get_data(test_path, split="test")
+			for model in models_by_name:
+				
+				testX, testY = split_test(test_docs, model)
+				trainX, trainY  = split(train_docs, model)
+				
+				trainX = np.asarray(trainX)
+				testX = np.asarray(testX)
+				trainY = np.asarray(trainY)
+				testY = np.asarray(testY)
+				trainX = reshapeX(trainX)
+				testX = reshapeX(testX)
+				trainY = reshapeY(trainY)
+				cnn = CNN(100).get_model()
+				cnn.fit(trainX, trainY,  epochs=20, batch_size=250, verbose=0)
+				pred = cnn.predict(testX)
+				val = []
+				for i in pred:
+					if i[0] > 0.50:
+						val.append(0)
+					else:
+						val.append(1)
+				
+				linear_model = SVC()
+				linear_model.fit(trainX, trainY)
+				val = linear_model.predict(testX)
+				test_accuracy = precision_recall_fscore_support(testY, val, average="weighted")
+
+				print "Accuracy = {0:.4f}".format( accuracy_score(testY, val))
+				print "Precision = {0:.4f}".format(test_accuracy[0])
+				print "Recall = {0:.4f}".format(test_accuracy[1])
+				print "F1-Score = {0:.4f}".format(test_accuracy[2])
+
 """
+#training with SVC
 for model in models_by_name:
 	trainX, trainY  = split(train_docs, model)
-	testX, testY = split(test_docs, model)
+	testX, testY = split_test(test_docs, model)
+	linear_model = SVC()
+	linear_model.fit(trainX, trainY)
+	pred = linear_model.predict(testX)
+	print accuracy_score(testY, pred)
+	print precision_recall_fscore_support(testY, pred, average="weighted")
+
+for model in models_by_name:
+	trainX, trainY  = split(train_docs, model)
+	testX, testY = split_test(test_docs, model)
 	trainX = np.asarray(trainX)
 	testX = np.asarray(testX)
 	trainY = np.asarray(trainY)
+	testY = np.asarray(testY)
 
 	trainX = reshapeX(trainX)
 	testX = reshapeX(testX)
 	trainY = reshapeY(trainY)
-	print len(trainX[0])
-	cnn = CNN(200).get_model()
-	cnn.fit(trainX, trainY,  nb_epoch=200, batch_size=250)
+	cnn = CNN(100).get_model()
+	cnn.fit(trainX, trainY,  epochs=20, batch_size=250, verbose=0)
 	pred = cnn.predict(testX)
-	pred = np_utils.probas_to_classes(pred)
-	test_accuracy = get_accuracy(pred, testY)
-	print test_accuracy
+	val = []
+	for i in pred:
+		if i[0] > 0.50:
+			val.append(0)
+		else:
+			val.append(1)
+	test_accuracy = precision_recall_fscore_support(testY, val, average="weighted")
+	print train, test_path
+	print "Accuracy = {0:.4f}".format( accuracy_score(testY, val))
+	print "Precision = {0:.4f}".format(test_accuracy[0])
+	print "Recall = {0:.4f}".format(test_accuracy[1])
+	print "F1-Score = {0:.4f}".format(test_accuracy[2])
 """
